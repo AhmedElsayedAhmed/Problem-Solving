b2818d9a1a87df286f9d535c686350bc6d934790
AhmedElsayedAhmed <fcih_ahmed@hotmail.com>
2019-05-01 12:00:02 +0200

    init
    init

---
+++ Word/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var lowerCase = 0;
+            var upperCase = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsUpper(input[i]))
+                    upperCase++;
+                else
+                    lowerCase++;
+            }
+
+            var result = (lowerCase >= upperCase) ? input.ToLower() : input.ToUpper();
+            Console.WriteLine(result);
+        }
+    }
+}

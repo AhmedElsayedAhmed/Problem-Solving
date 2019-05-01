6d22292d3f073be2e1bc167716f6e2ec0350fc2a
AhmedElsayedAhmed <fcih_ahmed@hotmail.com>
2019-05-01 14:52:01 +0200

    Aphabet

---
+++ Alphabet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var input = Console.ReadLine();
+            HashSet<char> chars = new HashSet<char>(input.ToLower());
+            if (chars.Count == 26)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
